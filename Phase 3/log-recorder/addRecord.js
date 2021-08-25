ffe1479185468b29f4315d592b50bfd3fa3003f1
Atush Bhalla <atushbhalla@email.arizona.edu>
2021-08-25 09:01:36 -0700

    Added debugger

--- Phase 3/log-recorder/addRecord.js
+++ Phase 3/log-recorder/addRecord.js
@@ -3,17 +3,21 @@ function addRecords(){
     let file = require("fs");
     let addRecords = true;
     let fname = readData.question("Enter First Name: ");
+    debugger;
     let lname = readData.question("Enter Last Name: ");
     let gender = readData.question("Enter Gender: ");
     let email = readData.question("Enter Email: ");
     let data = file.readFileSync("records.json");
+    debugger;
     let records = [];
     if(data.length){
         records = JSON.parse(data);
     }
     let newEmp = {"fname" : fname, "lname" : lname, "gender" : gender, "email" : email };
+    debugger;
     records.push(newEmp);
     file.writeFileSync("records.json", JSON.stringify(records));
+    debugger;
 }
 
 addRecords();
