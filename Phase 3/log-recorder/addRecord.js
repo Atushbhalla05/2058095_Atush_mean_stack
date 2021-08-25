0fee34cd0326fd15757458dd94193fbeb7517315
Atush Bhalla <atushbhalla@email.arizona.edu>
2021-08-25 09:00:58 -0700

    Used readline-sync

--- Phase 3/log-recorder/addRecord.js
+++ Phase 3/log-recorder/addRecord.js
@@ -1,29 +1,19 @@
 function addRecords(){
-    let obj = require("readline");
-    var readData = obj.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    })
-
+    let readData = require("readline-sync");
     let file = require("fs");
     let addRecords = true;
-    readData.question("Enter First Name: ", (fname) =>{
-        readData.question("Enter Last Name: ", (lname) =>{
-            readData.question("Enter Gender: ", (gender) =>{
-                readData.question("Enter Email: ", (email) =>{
-                    let data = file.readFileSync("records.json");
-                    let records = [];
-                    if(data.length){
-                        records = JSON.parse(data);
-                    }
-                    let newEmp = {"fname" : fname, "lname" : lname, "gender" : gender, "email" : email };
-                    records.push(newEmp);
-                    file.writeFileSync("records.json", JSON.stringify(records));
-                    readData.close();
-                 })
-            })
-        })            
-     })
+    let fname = readData.question("Enter First Name: ");
+    let lname = readData.question("Enter Last Name: ");
+    let gender = readData.question("Enter Gender: ");
+    let email = readData.question("Enter Email: ");
+    let data = file.readFileSync("records.json");
+    let records = [];
+    if(data.length){
+        records = JSON.parse(data);
+    }
+    let newEmp = {"fname" : fname, "lname" : lname, "gender" : gender, "email" : email };
+    records.push(newEmp);
+    file.writeFileSync("records.json", JSON.stringify(records));
 }
 
 addRecords();
